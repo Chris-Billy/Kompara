2b7b3d9696a609a8cf0e9e589368635204ce7a80
Billy Christopher <billy_christopher@hotmail.com>
2021-03-04 16:20:02 +0100

    Animation menu header

--- header.js
+++ header.js
@@ -1,11 +1,13 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('#menu');
 const logo = document.querySelector('#logo-burger');
+const nav = document.querySelector('#nav');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
     logo.classList.toggle('active');
+    nav.classList.toggle('active');
 });
 
 
