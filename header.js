43b08ab040d4cf808d7b61513a66a1908aebd50b
Christopher Billy <billy_christopher@hotmail.com>
2021-03-04 20:41:30 +0100

    Animation sur petit écran fonctionnel + ancrages page d'accueil

--- header.js
+++ header.js
@@ -2,12 +2,14 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('#menu');
 const logo = document.querySelector('#logo-burger');
 const nav = document.querySelector('#nav');
+const pusher = document.querySelector('.pusher');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
     logo.classList.toggle('active');
     nav.classList.toggle('active');
+    pusher.classList.toggle('active');
 });
 
 
