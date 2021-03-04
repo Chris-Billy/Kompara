01076faeb51df0ffc4deb70fa6db493bd7f3836c
Billy Christopher <billy_christopher@hotmail.com>
2021-03-04 15:05:44 +0100

    Menu burger terminé

--- header.js
+++ header.js
@@ -1,5 +1,11 @@
 const burger = document.querySelector('.burger');
+const menu = document.querySelector('#menu');
+const logo = document.querySelector('#logo-burger');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    logo.classList.toggle('active');
 });
+
+
