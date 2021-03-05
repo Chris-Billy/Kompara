e7ae7d78a4f892a6fa13258d2559a88fc72ea514
Billy Christopher <billy_christopher@hotmail.com>
2021-03-05 11:53:08 +0100

    Ajout début page histoire

--- header.js
+++ header.js
@@ -1,9 +1,11 @@
+// On récupère nos éléments html pour le menu burger
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('#menu');
 const logo = document.querySelector('#logo-burger');
 const nav = document.querySelector('#nav');
 const pusher = document.querySelector('.pusher');
 
+// On place un évenement d'écoute sur le menu burger
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
